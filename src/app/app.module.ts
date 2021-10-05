04e067da7eea9ea66ef9b547e3a90c8933886943
LuongMinh13 <l.minh1995@yahoo.fr>
2021-10-05 16:22:25 +0200

    about final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TestComponent } from './test/test.component';
+import { AboutComponent } from './about/about.component';
+import { OffrespecialComponent } from './offrespecial/offrespecial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    TestComponent,
+    AboutComponent,
+    OffrespecialComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/offrespecial/offrespecial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offrespecial',
+  templateUrl: './offrespecial.component.html',
+  styleUrls: ['./offrespecial.component.css']
+})
+export class OffrespecialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
